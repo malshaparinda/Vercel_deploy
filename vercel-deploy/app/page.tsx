b55fd7843ce53fa66891ec4e446a8701b57e4c5f
malshaparinda <malsha.parinda2000@gmail.com>
2026-03-06 13:22:15 +0530

    Initial commit

---
+++ vercel-deploy/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", fontSize: "34px"}}>
+      Hi Malsha , your first vercel deploy is running on vercel
+    </main>
+  );
+}
