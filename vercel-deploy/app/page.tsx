5523bbc46b6ffa4b90673a78c3f4e2941c86f3b7
malshaparinda <malsha.parinda2000@gmail.com>
2026-03-06 13:36:06 +0530

    update

--- vercel-deploy/app/page.tsx
+++ vercel-deploy/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", fontSize: "34px"}}>
       Hi Malsha , your first vercel deploy is running on vercel...
       Test pass
+      123445
     </main>
   );
 }
